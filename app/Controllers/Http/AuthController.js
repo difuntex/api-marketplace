a30a00ca1473eea12cafe570c2acde686b63a2d4
Rafael Teodoro <rafael.teodoro.do.nascimento@gmail.com>
2021-04-18 21:53:20 -0300

    Primeiro commit

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,30 @@
+"use strict";
+const User = use("App/Models/User");
+
+class AuthController {
+  async register({ request }) {
+    const data = request.only([
+      "username",
+      "email",
+      "password",
+      "users_tipo",
+      "telefone",
+      "cpf",
+      "data_nascimento",
+      "logradouro",
+      "complemento",
+      "numero",
+      "bairro",
+    ]);
+    const user = await User.create(data);
+    return user;
+  }
+  async authenticate({ request, auth }) {
+    const { email, password } = request.all();
+    const token = await auth.attempt(email, password);
+    return token;
+  }
+}
+
+module.exports = AuthController;
+
