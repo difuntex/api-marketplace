8d6958cc26d11594547c8170a5bd9f24b202222d
Rafael Teodoro <rafael.teodoro.do.nascimento@gmail.com>
2021-05-03 22:05:19 -0300

    CRUD das categorias concluido

--- app/Controllers/Http/CategoriaController.js
+++ app/Controllers/Http/CategoriaController.js
@@ -25,21 +25,42 @@ class CategoriaController {
     `;
     let categoriaExistente = await Database.raw(categoriaExistenteSQL);
     if (categoriaExistente.rows == []) {
-      console.log(categoriaExistente.rows)
+      console.log(categoriaExistente.rows);
       throw BadRequestException;
     }
     await Categoria.create(data);
     return { success: true, message: "Categoria cadastrada com sucesso" };
   }
-  async update(request,auth){
+  async update({ request, auth }) {
+    const user = await auth.getUser();
     if (user.users_tipo != 1) {
       throw UnauthorizedException;
     }
-    const data = request.only(["id","descricao"]);
-    
-
+    const data = request.only(["id", "descricao"]);
+    const categoria = await Categoria.findByOrFail("id", data.id);
+    //console.log(categoria);
+    delete data.id;
+    try {
+      categoria.merge(data);
+      await categoria.save();
+      return { success: true, message: "Categoria alterada com sucesso!" };
+    } catch (error) {
+      throw BadRequestException;
+    }
   }
-  
+  async delete({ request, auth }) {
+    const user = await auth.getUser();
+    if (user.users_tipo != 1) {
+      throw UnauthorizedException;
+    }
+    const data = request.only(["id"]);
+    const categoria = await Categoria.findByOrFail("id", data.id);
+    if (!categoria) {
+      throw BadRequestException;
+    }
+    await categoria.delete();
+    return { success: true, message: "A categoria foi excluida com sucesso!" };
+  }
 }
 
 module.exports = CategoriaController;
--- start/routes.js
+++ start/routes.js
@@ -8,6 +8,8 @@ Route.post("authenticate", "AuthController.authenticate");
 Route.get("/app", "AppController.index").middleware(["auth"]);
 
 Route.get("/categorias", "CategoriaController.index");
-Route.post("/categorias", "CategoriaController.store")
+Route.post("/categorias", "CategoriaController.store");
+Route.put("/categorias", "CategoriaController.update");
+Route.delete("/categorias", "CategoriaController.delete");
 
 Route.get("/produtos/:id", "ProdutoController.show");
